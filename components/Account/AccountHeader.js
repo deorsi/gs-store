08b7e55684d36adfb7c02a98958d5a0b93c8eb8a
deorsi <deorsi@protonmail.com>
2019-12-02 20:18:35 -0300

    Account header & order history

--- components/Account/AccountHeader.js
+++ components/Account/AccountHeader.js
@@ -1,5 +1,25 @@
-function AccountHeader() {
-  return <>AccountHeader</>;
+import { Header, Icon, Segment, Label } from "semantic-ui-react";
+import HeaderSubHeader from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
+
+function AccountHeader({ role, email, name, createdAt }) {
+  return (
+    <Segment secondary inverted color="violet">
+      <Label
+        color="green"
+        size="large"
+        ribbon
+        icon="privacy"
+        style={{ textTransform: "capitalize" }}
+        content={role}
+      />
+      <Header inverted textAlign="center" as="h1" icon>
+        <Icon name="user" />
+        {name}
+        <HeaderSubHeader>{email}</HeaderSubHeader>
+        <HeaderSubHeader>Entrou em {createdAt}</HeaderSubHeader>
+      </Header>
+    </Segment>
+  );
 }
 
 export default AccountHeader;
--- components/Account/AccountOrders.js
+++ components/Account/AccountOrders.js
@@ -1,5 +1,89 @@
-function AccountOrders() {
-  return <>AccountOrders</>;
+import {
+  Header,
+  Accordion,
+  Label,
+  Segment,
+  Icon,
+  List,
+  Image,
+  Button
+} from "semantic-ui-react";
+import { useRouter } from "next/router";
+
+function AccountOrders({ orders }) {
+  const router = useRouter();
+
+  function mapOrdersToPanels(orders) {
+    return orders.map(order => ({
+      key: order._id,
+      title: {
+        content: <Label color="violet" content={order.createdAt} />
+      },
+      content: {
+        content: (
+          <>
+            <List.Header as="h3">
+              Total: R${order.total}
+              <Label
+                content={order.email}
+                icon="mail"
+                basic
+                horizontal
+                style={{ marginLeft: "1em" }}
+              />
+            </List.Header>
+            <List>
+              {order.products.map(p => (
+                <List.Item>
+                  <Image avatar src={p.product.mediaUrl} />
+                  <List.Content>
+                    <List.Header>{p.product.name}</List.Header>
+                    <List.Description>
+                      {p.quantity} x ${p.product.price}
+                    </List.Description>
+                  </List.Content>
+                  <List.Content floated="right">
+                    <Label tag color="orange" size="tiny">
+                      {p.product.sku}
+                    </Label>
+                  </List.Content>
+                </List.Item>
+              ))}
+            </List>
+          </>
+        )
+      }
+    }));
+  }
+
+  return (
+    <>
+      <Header as="h2">
+        <Icon name="folder open" />
+        Histórico de Compra
+      </Header>
+      {orders.length === 0 ? (
+        <Segment inverted tertiary color="grey" textAlign="center">
+          <Header>
+            <Icon name="copy outline" />
+            Sem compras anteriores.
+          </Header>
+          <div>
+            <Button onClick={() => router.push("/")} color="orange">
+              Veja os Produtos
+            </Button>
+          </div>
+        </Segment>
+      ) : (
+        <Accordion
+          fluid
+          styled
+          exclusive={false}
+          panels={mapOrdersToPanels(orders)}
+        />
+      )}
+    </>
+  );
 }
 
 export default AccountOrders;
