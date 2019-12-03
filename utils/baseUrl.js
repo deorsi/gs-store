c1e25308b8f208c0c2a5ebd615575f776161501b
deorsi <deorsi@protonmail.com>
2019-12-02 22:36:54 -0300

    modification on baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://great-scott.now.sh"
+    ? "https://great-scott.herokuapp.com"
     : "http://localhost:3000";
 
 export default baseUrl;
